fd5c0bc4954efa3b5a4ea373ea86fe8b51d8d736
vallejocvl <vallejo.cvl@me.com>
2025-03-05 21:06:34 -0600

    Add Toss Coin

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ st.header('Lanzar una moneda')
 
 chart = st.line_chart([0.5])
 
-def toss_coin(n): # función que emula el lanzamiento de una moneda
+def toss_coin(n):
 
     trial_outcomes = scipy.stats.bernoulli.rvs(p=0.5, size=n)
 
@@ -29,3 +29,4 @@ start_button = st.button('Ejecutar')
 
 if start_button:
     st.write(f'Experimento con {number_of_trials} intentos en curso.')
+    mean = toss_coin(number_of_trials)
